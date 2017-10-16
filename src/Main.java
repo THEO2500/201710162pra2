ee72e25d3124294f6479080d782de13efb75856f
THEO2500 <worldpeace871219@gmail.com>
2017-10-16 21:26:10 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	char n='b';
+	n=in.next().charAt(0);
+		// 為甚麼不是n=in.nextChar();?
+	System.out.printf("%c\n",n);
+	System.out.printf("%d\n",(int)n);
+     //完全不知道這串事幹嘛用的
+
+    }
+}
